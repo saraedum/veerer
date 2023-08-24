2853e6ceb08b3961f18ab26ad8efa6a6cee79065
Julian Rüth <julian.rueth@fsfe.org>
2023-08-25 00:49:21 +0300

    Add better progress reporting

--- veering_triangulations_dask_futures.py
+++ veering_triangulations_dask_futures.py
@@ -51,32 +51,39 @@ def run_parallel(vt0):
     from dask.distributed import as_completed
     jobs = as_completed([pool.submit(geometric_neighbors_batched, [vt0])], with_results=True)
 
-    for completed in jobs:
-        _, results = completed
-        tasks = []
-        for result in results:
-            vt, vt_neighbors = result
-            for vt2 in vt_neighbors:
-                if vt2 not in graph:
-                    tasks.append(vt2)
-                    graph[vt2] = None
-            graph[vt] = vt_neighbors
-        if not tasks:
-            continue
-        if len(tasks) < 8:
-            batches = [tasks]
-        else:
-            batches = [tasks[:len(tasks)//2], tasks[len(tasks)//2:]]
-        for batch in batches:
-            jobs.add(pool.submit(geometric_neighbors_batched, batch))
+    from rich.progress import Progress, TextColumn, TimeElapsedColumn, BarColumn, MofNCompleteColumn
+    with Progress(TextColumn("{task.description}"), BarColumn(), TimeElapsedColumn(), MofNCompleteColumn(), transient=True, refresh_per_second=1) as progress:
+        task = progress.add_task("completing graph", total=len(graph))
 
-    return graph
+        for completed in jobs:
+            _, results = completed
+            tasks = []
+            for result in results:
+                vt, vt_neighbors = result
+                for vt2 in vt_neighbors:
+                    if vt2 not in graph:
+                        tasks.append(vt2)
+                        graph[vt2] = None
+                        progress.update(task, total=len(graph))
+                graph[vt] = vt_neighbors
+                progress.update(task, advance=1)
+            if not tasks:
+                continue
+            if len(tasks) < 8:
+                batches = [tasks]
+            else:
+                batches = [tasks[:len(tasks)//2], tasks[len(tasks)//2:]]
+            for batch in batches:
+                jobs.add(pool.submit(geometric_neighbors_batched, batch))
+
+        return graph
 
 
 if __name__ == '__main__':
     import dask.distributed
-    # pool = dask.distributed.Client()
-    pool = dask.distributed.Client("localhost:8786", direct_to_workers=False)
+    cores = 24
+    pool = dask.distributed.Client(n_workers=2*cores)
+    # pool = dask.distributed.Client("localhost:8786", direct_to_workers=False)
     # preload="dask_preload", serializers=["pickle"], deserializers=["pickle"], nworkers=8, nthreads=1)
 
     import sys
@@ -90,16 +97,22 @@ if __name__ == '__main__':
     # For a larger computation, replace the line below with one of
     # stratum_component = AbelianStratum(4).odd_component()
     # stratum_component = AbelianStratum(2, 2).hyperelliptic_component()
-    stratum_component = AbelianStratum(4).hyperelliptic_component()
+    # stratum_component = AbelianStratum(4).hyperelliptic_component()
+    # stratum_component = AbelianStratum(3, 1).unique_component()
+    # stratum_component = AbelianStratum(2, 2).hyperelliptic_component()
+    # stratum_component = AbelianStratum(2, 2).odd_component()
+    # stratum_component = AbelianStratum(6).hyperelliptic_component()
+    stratum_component = AbelianStratum(6).odd_component()
 
     import os
-    print("master is", os.getpid())
     print('Computing geometric veering triangulations in %s' % stratum_component)
     vt0 = VeeringTriangulation.from_stratum(stratum_component).copy(mutable=True)
     vt0.set_canonical_labels()
     vt0.set_immutable()
 
-    t0 = time()
+    import datetime
+    t0 = datetime.datetime.now()
     graph = run_parallel(vt0)
-    t1 = time()
-    print('%d triangulations computed in %f' % (len(graph), t1 - t0))
+    t1 = datetime.datetime.now()
+    elapsed = t1 - t0
+    print(f'{len(graph)} triangulations computed in {elapsed * 2 * cores} CPU time')
