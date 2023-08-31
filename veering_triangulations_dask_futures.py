382cd7d4de5c01270c75f42373d18d9a54db79d9
Julian Rüth <julian.rueth@fsfe.org>
2023-08-31 06:52:33 +0300

    Revert "Try to make the dask runner more efficient (and fail at it.)"
    This reverts commit 560af5fa9681ae1b7d8df23137c524a90c98e826.

--- veering_triangulations_dask_futures.py
+++ veering_triangulations_dask_futures.py
@@ -1,114 +1,95 @@
 import sage.all
-import pickle
-import dask.distributed
+r"""
+Tiings for the computation of the geometric veering triangulations
+of H(4)^hyp
 
 
+Usage
+
+To run it sequentially
+
+   $ sage -python veering_triangulations_multiprocessing.py s
+
+To run it in parallel with 4 processes
+
+   $ sage -python veering_triangulations_multiprocessing.py 4
+
+Twice sequentially and then 3 and 4 processes
+
+   $ sage -python veering_triangulations_multiprocessing.py s 3 4
+"""
 def geometric_neighbors(vt):
     r"""
     Return the pair ``(vt, list_of_neighbors)`` that consists of the input
     triangulation ``vt`` together with the list ``list_of_neighbors`` of
     neighbors in the flip graph.
     """
-    assert not hasattr(vt, "result")
-    if isinstance(vt, bytes):
-        vt = pickle.loads(vt)
-
-    neighbors = []
-
+    ans = []
     for edges, col in vt.geometric_flips(backend='ppl'):
-        neighbor = vt.copy(mutable=True)
+        new_vt = vt.copy(mutable=True)
         for e in edges:
-            neighbor.flip(e, col, check=False)
-        neighbor.set_canonical_labels()
-        neighbor.set_immutable()
-        neighbors.append(neighbor)
-
-    return neighbors
-
-
-def md5(vt):
-    assert not hasattr(vt, "result")
-    if not isinstance(vt, bytes):
-        vt = pickle.dumps(vt)
-
-    import hashlib
-    return hashlib.md5(vt).digest()
+            new_vt.flip(e, col, check=False)
+        new_vt.set_canonical_labels()
+        new_vt.set_immutable()
+        ans.append(new_vt)
+    return vt, ans
 
 
-def md5s(vts):
-    return [md5(vt) for vt in vts]
+def geometric_neighbors_batched(vts):
+    return [geometric_neighbors(vt) for vt in vts]
 
 
-def select(vts, i):
-    return vts[i]
+def run_parallel(vt0):
+    r"""
+    Compute the graph using a multiprocessing.Pool
 
+    NOTE: the maxtaskperchild argument to Pool is randomly set to 128 so
+    that workers are restarted to avoid leaking
+    """
+    graph = {vt0: []}
 
-def run_parallel(roots, pool, graph, threads):
-    keys = md5s(roots)
-    roots = pool.scatter(roots)
-
-    unprocessed = list(zip(keys, roots))
-
-    seen = set(keys)
-    processing = {}
+    from dask.distributed import as_completed
+    jobs = as_completed([pool.submit(geometric_neighbors_batched, [vt0])], with_results=True)
 
     from rich.progress import Progress, TextColumn, TimeElapsedColumn, BarColumn, MofNCompleteColumn
     with Progress(TextColumn("{task.description}"), BarColumn(), TimeElapsedColumn(), MofNCompleteColumn(), transient=True, refresh_per_second=1) as progress:
-        progress_bar = progress.add_task("completing graph", total=len(unprocessed))
+        task = progress.add_task("completing graph", total=len(graph))
 
-        while processing or unprocessed:
-            print(f"processing {len(processing)}, unprocessed {len(unprocessed)}, seen {len(seen)}")
+        for completed in jobs:
+            _, results = completed
+            tasks = []
+            for result in results:
+                vt, vt_neighbors = result
+                for vt2 in vt_neighbors:
+                    if vt2 not in graph:
+                        tasks.append(vt2)
+                        graph[vt2] = None
+                        progress.update(task, total=len(graph))
+                graph[vt] = vt_neighbors
+                progress.update(task, advance=1)
+            if not tasks:
+                continue
+            if len(tasks) < 8:
+                batches = [tasks]
+            else:
+                batches = [tasks[:len(tasks)//2], tasks[len(tasks)//2:]]
+            for batch in batches:
+                jobs.add(pool.submit(geometric_neighbors_batched, batch))
 
-            while unprocessed and len(processing) < 8 * threads:
-                key, vt = unprocessed.pop()
-                neighbors = pool.submit(geometric_neighbors, vt)
-                processing[neighbors] = (geometric_neighbors, (vt, key))
-
-            if processing:
-                import concurrent.futures
-
-                completed, _ = dask.distributed.wait(list(processing.keys()), return_when=concurrent.futures.FIRST_COMPLETED)
-
-                for task in completed:
-                    kind, args = processing.pop(task)
-                    workers = pool.who_has(task)[task.key]
-
-                    if kind == geometric_neighbors:
-                        vt, key = args
-                        vts = task
-
-                        processing[pool.submit(md5s, vts, workers=workers)] = (md5s, (vt, key, vts))
-
-                    if kind == md5s:
-                        vt, key, vts = args
-                        vt = vt.result()
-
-                        keys = task.result()
-
-                        assert key not in graph
-
-                        graph[key] = (vt, keys)
-
-                        for i, key in enumerate(keys):
-                            if key not in seen:
-                                unprocessed.append((key, pool.submit(select, vts, i, workers=workers)))
-                                seen.add(key)
-
-                        progress.update(progress_bar, total=len(seen))
-                        progress.update(progress_bar, advance=1)
-
-    return graph
+        return graph
 
 
-def main():
-
-    import os
-    threads = os.cpu_count()
-
-    pool = dask.distributed.Client(n_workers=threads)
+if __name__ == '__main__':
+    import dask.distributed
+    cores = 24
+    pool = dask.distributed.Client(n_workers=2*cores)
     # pool = dask.distributed.Client("localhost:8786", direct_to_workers=False)
     # preload="dask_preload", serializers=["pickle"], deserializers=["pickle"], nworkers=8, nthreads=1)
 
+    import sys
+    from time import sleep, time
+
+    import sage.all
     from veerer import VeeringTriangulation
     from surface_dynamics import AbelianStratum
 
@@ -116,13 +97,14 @@ def main():
     # For a larger computation, replace the line below with one of
     # stratum_component = AbelianStratum(4).odd_component()
     # stratum_component = AbelianStratum(2, 2).hyperelliptic_component()
-    stratum_component = AbelianStratum(4).hyperelliptic_component()
+    # stratum_component = AbelianStratum(4).hyperelliptic_component()
     # stratum_component = AbelianStratum(3, 1).unique_component()
     # stratum_component = AbelianStratum(2, 2).hyperelliptic_component()
     # stratum_component = AbelianStratum(2, 2).odd_component()
     # stratum_component = AbelianStratum(6).hyperelliptic_component()
-    # stratum_component = AbelianStratum(6).odd_component()
+    stratum_component = AbelianStratum(6).odd_component()
 
+    import os
     print('Computing geometric veering triangulations in %s' % stratum_component)
     vt0 = VeeringTriangulation.from_stratum(stratum_component).copy(mutable=True)
     vt0.set_canonical_labels()
@@ -130,11 +112,7 @@ def main():
 
     import datetime
     t0 = datetime.datetime.now()
-    graph = run_parallel([vt0], pool, graph={}, threads=threads)
+    graph = run_parallel(vt0)
     t1 = datetime.datetime.now()
     elapsed = t1 - t0
-    print(f'{len(graph)} triangulations computed in {elapsed * threads} CPU time')
-
-
-if __name__ == '__main__':
-    main()
+    print(f'{len(graph)} triangulations computed in {elapsed * 2 * cores} CPU time')
