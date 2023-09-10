efc4f4fca45990bd4722d252e5e22adab0464c0e
Julian Rüth <julian.rueth@fsfe.org>
2023-09-10 10:22:00 +0200

    Make dask exploration async and functional again

--- veerer/veering_triangulations_dask_futures.py
+++ veerer/veering_triangulations_dask_futures.py
@@ -4,9 +4,6 @@ import os
 import asyncio
 
 
-BATCH_LEN = 48
-
-
 def dumps(*args, **kwargs):
     return compress_pickle.dumps(*args, compression="bz2", **kwargs)
 
@@ -38,97 +35,115 @@ def md5(vt):
     return hashlib.md5(vt).digest()
 
 
-async def explore(roots, pool, threads, graph, seen=None, completed=0, backend='ppl'):
-    if seen is None:
-        seen = set()
+class Explorer:
+    def __init__(self, graph, seen, pool, threads, backend='ppl'):
+        self._to_be_scheduled = []
+        self._pending_batches = []
+        self._graph = graph
+        self._seen = seen
+        self._pool = pool
+        self._threads = threads
+        self._backend = backend
 
-    def is_new(vt):
+    async def explore(self, roots):
+        self._to_be_scheduled.extend(roots)
+        self._scheduler_wakeup = asyncio.Future()
+
+        from rich.progress import Progress, TextColumn, TimeElapsedColumn, BarColumn, MofNCompleteColumn
+        with Progress(TextColumn("{task.description}"), BarColumn(), TimeElapsedColumn(), MofNCompleteColumn(), transient=True, refresh_per_second=5) as progress:
+            self._progress = progress
+            self._task_exploring = progress.add_task("exploring graph")
+            self._task_scheduling = progress.add_task("scheduling explorations")
+            self._task_consuming = progress.add_task("computing neighbors")
+            self._update_progress()
+
+            _scheduler = asyncio.create_task(self._schedule())
+            _consumer = asyncio.create_task(self._consume())
+
+            completed, pending = await asyncio.wait([_scheduler, _consumer], return_when=asyncio.FIRST_COMPLETED)
+            for task in completed:
+                await task
+            for task in pending:
+                task.cancel()
+
+    def _schedule_nbatches(self):
+        ntasks = len(self._to_be_scheduled)
+
+        if ntasks == 0:
+            return "WAIT"
+
+        # We assume that everything in _pending_batches is actually being computed currently.
+        # So _threads many batches are being computed (but presumably almost
+        # done) and another _threads many are going to be computed shortly.
+        # We aim for there to be a buffer of another threads many batches that can be picked up by workers.
+        TARGET_BATCHES = 3 * self._threads
+        batch_count_from_TARGET_BATCHES = max(1, TARGET_BATCHES - len(self._pending_batches))
+
+        # We do not want batches to become too big as this causes delays in
+        # network communication.
+        TARGET_BATCH_SIZE = 32
+        batch_count_from_TARGET_BATCH_SIZE = max(1, ntasks // TARGET_BATCH_SIZE)
+
+        if batch_count_from_TARGET_BATCHES == 1 and batch_count_from_TARGET_BATCH_SIZE < self._threads // 2:
+            # There's still enough pending batches in the queue. Wait a bit for more work to hand to accumulate.
+            return "WAIT"
+
+        return min(max(batch_count_from_TARGET_BATCHES, batch_count_from_TARGET_BATCH_SIZE), ntasks)
+
+    async def _schedule(self):
+        while True:
+            nbatches = self._schedule_nbatches()
+            if nbatches == "WAIT":
+                self._scheduler_wakeup = asyncio.Future()
+                await self._scheduler_wakeup
+                continue
+
+            to_be_scheduled = [(arg,) for arg in self._to_be_scheduled]
+            self._to_be_scheduled = []
+
+            batches = [to_be_scheduled[offset::nbatches] for offset in range(nbatches)]
+            assert all(batch for batch in batches)
+
+            from veerer.worker import Batched
+            from veerer.veering_triangulations_dask_futures import geometric_neighbors
+            self._pending_batches.extend(self._pool.map(Batched(geometric_neighbors, backend=self._backend), batches))
+
+            self._update_progress()
+
+    async def _consume(self):
+        while True:
+            while self._pending_batches:
+                completed, pending = await asyncio.wait(self._pending_batches[:self._threads], return_when=asyncio.FIRST_COMPLETED)
+                self._pending_batches = list(pending) + self._pending_batches[len(completed) + len(pending):]
+
+                for batch in completed:
+                    for result in batch.result():
+                        vt, neighbors = result
+                        for neighbor in neighbors:
+                            self._register_node(neighbor)
+                        self._graph[vt] = b"".join(md5(neighbor) for neighbor in neighbors)
+
+                self._update_progress()
+
+            if not self._to_be_scheduled:
+                return
+
+            await asyncio.sleep(0)
+
+    def _update_progress(self):
+        self._progress.update(self._task_exploring, completed=len(self._graph), total=len(self._seen), visible=True)
+        self._progress.update(self._task_scheduling, completed=0, total=len(self._to_be_scheduled), visible=True)
+        self._progress.update(self._task_consuming, completed=0, total=len(self._pending_batches), visible=True)
+
+    def _register_node(self, vt):
+        if not self._scheduler_wakeup.done():
+            self._scheduler_wakeup.set_result("progress")
+
         key = md5(vt)
-        if key in seen:
-            return False
-        seen.add(key)
-        return True
-
-    assert all(is_new(vt) for vt in roots)
-
-    from dask.distributed import as_completed
-    jobs = as_completed([], with_results=True)
-    jobs = []
-
-    async def enqueue(tasks):
-        nonlocal submitted_jobs, progress
-
-        if not tasks:
+        if key in self._seen:
             return
-
-        progress.reset(task_sending)
-        progress.update(task_sending, completed=0, total=len(tasks), visible=True)
-
-        # We want there to always be 2 * threads jobs in the queue.
-        nbatches_from_threads = max(1, 2 * threads - (submitted_jobs - finished_jobs))
-        # We do not want to ship any tasks that contain more than BATCH_LEN jobs.
-        nbatches_from_package_size = max(1, len(tasks) // BATCH_LEN)
-
-        nbatches = max(nbatches_from_threads, nbatches_from_package_size)
-        nbatches = min(len(tasks), nbatches)
-
-        batches = [tasks[offset::nbatches] for offset in range(nbatches)]
-        batches = await pool.scatter(batches)
-        await pool.replicate(batches, n=2)
-
-        from veerer.worker import Batched
-        from veerer.veering_triangulations_dask_futures import geometric_neighbors
-        jobs.extend(pool.map(Batched(geometric_neighbors, backend=backend), batches))
-
-        submitted_jobs += len(batches)
-        progress.update(task_jobs, total=submitted_jobs)
-        progress.update(task_sending, visible=False)
-
-    submitted_jobs = 1
-    finished_jobs = 0
-
-    from rich.progress import Progress, TextColumn, TimeElapsedColumn, BarColumn, MofNCompleteColumn
-    with Progress(TextColumn("{task.description}"), BarColumn(), TimeElapsedColumn(), MofNCompleteColumn(), transient=True, refresh_per_second=1) as progress:
-        task_exploring = progress.add_task("exploring graph", completed=completed, total=len(seen), visible=True)
-        task_jobs = progress.add_task("processing batched jobs", total=submitted_jobs, visible=True)
-        task_batching = progress.add_task("...", total=0, visible=False)
-        task_sending = progress.add_task("sending tasks to workers", visible=False)
-
-        await enqueue([(root,) for root in roots])
-
-        while jobs:
-            tasks = []
-
-            progress.reset(task_batching)
-            progress.update(task_batching, description="waiting for finished batches", total=1, completed=0, visible=True)
-            completed, _ = await asyncio.wait(jobs)
-            jobs = []
-            # progress.update(task_batching, description="loading data for finished batches", total=len(completed), completed=0)
-            completed = await pool.gather(completed)
-
-            progress.update(task_batching, total=sum(len(result[1]) for batch in completed for result in batch), completed=0)
-            for batch in completed:
-                batch = batch.result()
-                progress.update(task_batching, description="processing discovered triangulations")
-                for result in batch:
-                    vt, vt_neighbors = result
-                    for vt2 in vt_neighbors:
-                        if is_new(vt2):
-                            tasks.append((vt2,))
-                            progress.update(task_exploring, total=len(seen))
-                        progress.update(task_batching, advance=1)
-                    graph[vt] = b"".join(md5(neighbor) for neighbor in vt_neighbors)
-                    progress.update(task_exploring, advance=1)
-                finished_jobs += 1
-                progress.update(task_jobs, advance=1)
-
-                if len(tasks) >= BATCH_LEN * 2 * threads:
-                    await enqueue(tasks)
-                    tasks = []
-
-            await enqueue(tasks)
-
-    return len(seen)
+        self._seen.add(key)
+        self._to_be_scheduled.append(vt)
 
 
 def loose_ends(db):
@@ -173,52 +188,6 @@ def loose_ends(db):
     return list(loose_ends), seen
 
 
-async def _main(recover, threads, scheduler, database, stratum_component, backend):
-    import dask.config
-    dask.config.set({'distributed.worker.daemon': False})
-    import dask.distributed
-    pool = await dask.distributed.Client(scheduler_file=scheduler, direct_to_workers=True, connection_limit=2**16, asynchronous=True)
-
-    from veerer import VeeringTriangulation
-    from surface_dynamics import AbelianStrata, QuadraticStrata
-
-    if stratum_component == 3413:
-        from veerer.linear_family import VeeringTriangulationLinearFamilies
-
-        print('Computing geometric veering of the (3, 4, 13) triangle')
-        vt = VeeringTriangulationLinearFamilies.triangle_3_4_13_unfolding_orbit_closure().copy(mutable=True)
-    else:
-        stratum_component = ([C for d in range(6, 9) for H in AbelianStrata(dimension=d) for C in H.components()] + [C for d in range(3, 9) for Q in QuadraticStrata(dimension=d, nb_poles=0) for C in Q.components()])[stratum_component]
-
-        print('Computing geometric veering triangulations in %s' % stratum_component)
-        vt = VeeringTriangulation.from_stratum(stratum_component).copy(mutable=True)
-        while not vt.is_geometric():
-            print(".", end="")
-            vt.random_forward_flip()
-    vt.set_canonical_labels()
-    vt.set_immutable()
-
-    roots = [dumps(vt)]
-    seen = None
-
-    import dbm
-    if recover:
-        with dbm.open(database, 'r') as graph:
-            roots, seen = loose_ends(graph)
-
-        if not roots and not seen:
-            roots = [dumps(vt)]
-
-    with dbm.open(database, 'c' if recover else 'n') as graph:
-        import datetime
-        t0 = datetime.datetime.now()
-        nodes = await explore(roots=roots, pool=pool, threads=threads, graph=graph, seen=seen, completed=max(0, len(graph) - len(roots)), backend=backend)
-        t1 = datetime.datetime.now()
-        elapsed = t1 - t0
-        print(f'{nodes} triangulations computed in {elapsed * threads} CPU time; {elapsed} wall time')
-    await pool.close()
-
-
 @click.command()
 @click.option('--recover/--no-recover', default=False, help='Continue from a previous aborted run.')
 @click.option('--threads', default=os.cpu_count(), help='The number of simultaneous hyperthreads for statistical purposes.')
@@ -227,7 +196,55 @@ async def _main(recover, threads, scheduler, database, stratum_component, backen
 @click.option('--stratum-component', default=0, type=int)
 @click.option('--backend', default='ppl')
 def main(recover, threads, scheduler, database, stratum_component, backend):
-    asyncio.run(_main(recover, threads, scheduler, database, stratum_component, backend))
+    async def main():
+        nonlocal stratum_component
+
+        import dask.config
+        dask.config.set({'distributed.worker.daemon': False})
+        import dask.distributed
+        pool = await dask.distributed.Client(scheduler_file=scheduler, direct_to_workers=True, connection_limit=2**16, asynchronous=True, n_workers=8, nthreads=1)
+
+        from veerer import VeeringTriangulation
+        from surface_dynamics import AbelianStrata, QuadraticStrata
+
+        if stratum_component == 3413:
+            from veerer.linear_family import VeeringTriangulationLinearFamilies
+
+            print('Computing geometric veering of the (3, 4, 13) triangle')
+            vt = VeeringTriangulationLinearFamilies.triangle_3_4_13_unfolding_orbit_closure().copy(mutable=True)
+        else:
+            stratum_component = ([C for d in range(6, 9) for H in AbelianStrata(dimension=d) for C in H.components()] + [C for d in range(3, 9) for Q in QuadraticStrata(dimension=d, nb_poles=0) for C in Q.components()])[stratum_component]
+
+            print('Computing geometric veering triangulations in %s' % stratum_component)
+            vt = VeeringTriangulation.from_stratum(stratum_component).copy(mutable=True)
+            while not vt.is_geometric():
+                print(".", end="")
+                vt.random_forward_flip()
+        vt.set_canonical_labels()
+        vt.set_immutable()
+
+        roots = [dumps(vt)]
+        seen = set()
+
+        import dbm
+        if recover:
+            with dbm.open(database, 'r') as graph:
+                roots, seen = loose_ends(graph)
+
+            if not roots and not seen:
+                roots = [dumps(vt)]
+
+        with dbm.open(database, 'c' if recover else 'n') as graph:
+            import datetime
+            t0 = datetime.datetime.now()
+
+            await Explorer(graph=graph, seen=seen, pool=pool, backend=backend, threads=threads).explore(roots=roots)
+            t1 = datetime.datetime.now()
+            elapsed = t1 - t0
+            print(f'{len(graph)} triangulations computed in {elapsed * threads} CPU time; {elapsed} wall time')
+        await pool.close()
+
+    asyncio.run(main())
 
 
 if __name__ == '__main__':
